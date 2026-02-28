a41e0b6f07447e8f4caeab3ae21239ff855fbb4f
santhusrk15-cloud <santhu.srk15@gmail.com>
2026-03-01 03:13:05 +0530

    Create constants.py

---
+++ traffic_system/traffic_system/constants.py
@@ -0,0 +1,12 @@
+HELMET_CLASSES = {
+    "driver_with_helmet",
+    "driver_without_helmet",
+    "passenger_with_helmet",
+    "passenger_without_helmet",
+}
+
+VIOLATION_SECTION_194D = "Section 194D"
+DEFAULT_PENALTY_194D = 1000
+
+PLATE_CLASS_NAME = "number_plate"
+MIN_INDIAN_PLATE_LENGTH = 8
